2851f0c7195b79cd1fde67617a2938040d7bf34d
Richard R. Araújo <richardrodriguis09@gmail.com>
2026-09-25 17:42:59 -0300

    feat: build BRASA page structure

---
+++ src/components/brasa/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="brasa-footer" id="contato">
+      <div className="brasa-footer__brand">BRASA</div>
+      <div className="brasa-footer__info"><span>COZINHA BRASILEIRA CONTEMPORÂNEA</span><a href="#menu">MENU</a><a href="#reservas">RESERVAS</a><a href="#casa">LOCALIZAÇÃO</a></div>
+      <div className="brasa-footer__bottom"><span>© 2026 BRASA</span><a href="#top">VOLTAR AO TOPO ↑</a></div>
+    </footer>
+  )
+}
